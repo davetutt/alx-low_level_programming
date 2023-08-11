7f4d8f971aaf24df05fc55282045e206391ef522
davetutt <davetutt946@gmail.com>
2023-08-10 22:04:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Return: 0 (success)
+ * Return: 1 (success)
  *
  */
 
 int main(void)
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
+	write("and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n");
 	return (1);
 }
