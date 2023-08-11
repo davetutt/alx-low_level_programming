6411c9bbf98fe322b6672156f37e0da791022083
davetutt <davetutt946@gmail.com>
2023-08-10 21:03:45 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - printing of variables
+ *
+ * return 0 (success)
+ *
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of char: %lu byte(s)\n", sizeof(a);
+			printf("Size of int: %lu byte(s)\n", sizeof(b);
+				printf("Size of long int: %lu byte(s)\n", sizeof(c);
+					printf("Size of long long int: %lu byte(s)\n", sizeof(d);
+						printf("Size of float: %lu byte(s)\n", sizeof(e);
+							return (0);
+}
