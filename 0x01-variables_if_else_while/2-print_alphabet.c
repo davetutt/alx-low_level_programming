c1e157183497db69ee8da028c44aa87f81a17200
davetutt <davetutt946@gmail.com>
2023-08-11 21:47:20 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Printing of alphabets
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
