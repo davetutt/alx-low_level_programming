c8e842dffb29ad5a051cda365a1612ccfc64be99
davetutt <davetutt946@gmail.com>
2023-08-13 12:58:54 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - all digits of base 10 using putchar
+ *
+ * Return: 0 (successe)
+ */
+
+int main(void)
+{
+	char k;
+
+	for (k = 0; k < 10; k++)
+	{
+		putchar((k % 10) + 'O');
+
+		putchar('\n');
+	}
+
+	return (0);
+}
