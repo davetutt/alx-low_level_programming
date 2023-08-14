0c0b8b325a8586b33681295bb5670a7f06faba1d
davetutt <davetutt946@gmail.com>
2023-08-13 19:55:17 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,9 @@ int main(void)
 	int num;
 	char y;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar((num % 10) + 'o');
+		putchar((num % 10) + '0');
 	}
 
 	for (y = 'a'; y <= 'f'; y++)
