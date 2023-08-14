36814531f60beade7c201c7bb243321354ef04cc
davetutt <davetutt946@gmail.com>
2023-08-13 19:46:21 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - printing numbers seperated by , and space
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		if (y == 9)
+			continue;
+
+		putchar(',');
+
+		putchar(' ');
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
