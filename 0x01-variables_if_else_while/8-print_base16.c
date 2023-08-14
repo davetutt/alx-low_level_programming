f931dcf4ad0eea46162c4ae8930459aa9c2cf75d
davetutt <davetutt946@gmail.com>
2023-08-13 19:22:13 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - printing numbers of base 16 in lowercase
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+	char y;
+
+	for (num = 0; num <= 10; num++)
+	{
+		putchar(num % 10 + '0');
+	}
+
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar(y);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
